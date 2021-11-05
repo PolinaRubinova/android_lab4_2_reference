8a07acb61b793eb5ef274676fe63d67903970d1d
PolinaRubinova <jerrygonsales@mail.ru>
2021-11-05 22:16:31 +0300

    Initial commit

---
+++ app/src/androidTest/java/com/example/myapplication/NavigationTest.kt
@@ -0,0 +1,247 @@
+package com.example.myapplication
+
+import androidx.test.core.app.launchActivity
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.assertion.ViewAssertions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+import android.content.pm.ActivityInfo
+import androidx.test.espresso.Espresso.*
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.doesNotExist
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.junit.Rule
+import java.lang.Thread.sleep
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class NavigationTest {
+    private fun delay() {sleep(1000)}
+
+    @get:Rule
+    val activityScenarioRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test
+    fun testAbout() {
+        launchActivity<MainActivity>()
+        openAbout()
+        Espresso.onView(ViewMatchers.withId(R.id.activity_about))
+            .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
+    }
+
+    @Test
+    fun existenceOfElementsTestFr1() {
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).check(doesNotExist())
+        onView(withId(R.id.bnToSecond)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).check(doesNotExist())
+        onView(withId(R.id.nav_view)).check(matches(isDisplayed()))
+//        onView(withId(R.id.text1)).check(isCompletelyAbove(withId(R.id.bnToSecond)))
+//        onView(withId(R.id.bnToSecond)).check(isCompletelyAbove(withId(R.id.nav_view)))
+
+    }
+
+    @Test
+    fun existenceOfElementsTestFr2() {
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).check(doesNotExist())
+        onView(withId(R.id.bnToThird)).check(matches(isDisplayed()))
+        onView(withId(R.id.nav_view)).check(matches(isDisplayed()))
+//        onView(withId(R.id.text2)).check(isCompletelyAbove(withId(R.id.bnToFirst)))
+//        onView(withId(R.id.bnToFirst)).check(isCompletelyAbove(withId(R.id.bnToThird)))
+//        onView(withId(R.id.bnToThird)).check(isCompletelyAbove(withId(R.id.nav_view)))
+    }
+
+    @Test
+    fun existenceOfElementsTestFr3() {
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).check(doesNotExist())
+        onView(withId(R.id.nav_view)).check(matches(isDisplayed()))
+//        onView(withId(R.id.text3)).check(isCompletelyAbove(withId(R.id.bnToFirst)))
+//        onView(withId(R.id.bnToFirst)).check(isCompletelyAbove(withId(R.id.bnToSecond)))
+//        onView(withId(R.id.bnToSecond)).check(isCompletelyAbove(withId(R.id.nav_view)))
+
+    }
+
+    @Test
+    fun existenceOfElementsTestAbout() {
+        openAbout()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvAbout)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).check(doesNotExist())
+        onView(withId(R.id.bnToSecond)).check(doesNotExist())
+        onView(withId(R.id.bnToThird)).check(doesNotExist())
+    }
+
+    @Test
+    fun backstackDepthTest() {
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.bnToFirst)).perform(click())
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.bnToFirst)).perform(click())
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+
+        openAbout()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+
+        pressBack()
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+        pressBack()
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        pressBack()
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun navigationForwardTest() {
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+
+        openAbout()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+        pressBack()
+
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+
+        openAbout()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+        pressBack()
+
+        onView(withId(R.id.bnToFirst)).perform(click())
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+
+        openAbout()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+        pressBack()
+
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+
+        openAbout()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+        pressBack()
+
+        onView(withId(R.id.bnToFirst)).perform(click())
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun changeScreenOrientationTestFr1() {
+        val scenario = activityScenarioRule.scenario
+
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+        scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+        delay()
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+
+        scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+        delay()
+        onView(withId(R.id.fragment1)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun changeScreenOrientationTestFr2() {
+        val scenario = activityScenarioRule.scenario
+
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+        delay()
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).check(doesNotExist())
+        onView(withId(R.id.bnToThird)).check(matches(isDisplayed()))
+        onView(withId(R.id.nav_view)).check(matches(isDisplayed()))
+
+        scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+        delay()
+        onView(withId(R.id.fragment2)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun changeScreenOrientationTestFr3() {
+        val scenario = activityScenarioRule.scenario
+
+        onView(withId(R.id.bnToSecond)).perform(click())
+        onView(withId(R.id.bnToThird)).perform(click())
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+        scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+        delay()
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToSecond)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToThird)).check(doesNotExist())
+        onView(withId(R.id.nav_view)).check(matches(isDisplayed()))
+
+        scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+        delay()
+        onView(withId(R.id.fragment3)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun changeScreenOrientationTestAbout() {
+        val scenario = activityScenarioRule.scenario
+
+        openAbout()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+        scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        }
+        delay()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+        onView(withId(R.id.tvAbout)).check(matches(isDisplayed()))
+        onView(withId(R.id.bnToFirst)).check(doesNotExist())
+        onView(withId(R.id.bnToSecond)).check(doesNotExist())
+        onView(withId(R.id.bnToThird)).check(doesNotExist())
+
+        scenario.onActivity { activity ->
+            activity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+        }
+        delay()
+        onView(withId(R.id.activity_about)).check(matches(isDisplayed()))
+    }
+}
